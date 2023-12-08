2695f9e6040f40a3bfa6ffa48aba7abe6a36a050
LucasheriqueDF <lucasdellarmelindo999@gmail.com>
2023-12-08 13:54:03 -0400

    chore: add dependecies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import 'dotenv/config';
+
+console.log(process.env.DATABASE_URL);
+
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
-    await app.listen(3000);
+    await app.listen(4000);
 }
 bootstrap();
